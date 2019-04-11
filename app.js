2def8f91867f45a023535b8079baaabe1476f90a
Rohan Joshi <joshi.rohan09@gmail.com>
2019-04-11 12:41:23 -0400

    added mongodb with basic data persistance

--- app.js
+++ app.js
@@ -2,40 +2,70 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const port = 3000
+const mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost/yelp_camp");
+
+//schemaSetup
+
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "Lakeside Woodfire Camp",
+//     image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"
+//   }, function(err, campground){
+//     if (err){
+//       console.log(err);
+//     } else {
+//       console.log("Newly created campground");
+//       console.log(campground);
+//     }
+//   });
 
 
-var campgrounds = [
-  {
-    name: "Lakeside Woodfire Camp",
-    image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"
-  },
-  {
-    name: "Mountain Skyline View",
-    image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"
-  },
-  {
-    name: "Appalacian Hikes",
-    image: "https://farm4.staticflickr.com/3290/3753652230_8139b7c717.jpg"
-  },
-  {
-    name: "Shenandoah Drive",
-    image: "https://farm9.staticflickr.com/8041/7930201874_6c17ed670a.jpg"
-  },
-  {
-    name: "Chutiya Campsite",
-    image: "https://farm4.staticflickr.com/3290/3753652230_8139b7c717.jpg"
-  }
-];
+// var campgrounds = [
+//   {
+//     name: "Lakeside Woodfire Camp",
+//     image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"
+//   },
+//   {
+//     name: "Mountain Skyline View",
+//     image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg"
+//   },
+//   {
+//     name: "Appalacian Hikes",
+//     image: "https://farm4.staticflickr.com/3290/3753652230_8139b7c717.jpg"
+//   },
+//   {
+//     name: "Shenandoah Drive",
+//     image: "https://farm9.staticflickr.com/8041/7930201874_6c17ed670a.jpg"
+//   },
+//   {
+//     name: "Chutiya Campsite",
+//     image: "https://farm4.staticflickr.com/3290/3753652230_8139b7c717.jpg"
+//   }
+// ];
 
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
 app.get("/campgrounds", (req,res) => {
-  res.render("campgrounds", {campgrounds:campgrounds});
+  Campground.find({}, (err, allCampgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("campgrounds", {campgrounds:allCampgrounds});
+    }
+  });
 });
 
 
@@ -47,9 +77,14 @@ app.post("/campgrounds", (req,res) => {
       name: name,
       image: imageUrl
     }
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
-
+    //campgrounds.push(newCampground);
+    Campground.create(newCampground, function(err, newlyCreated){
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect("/campgrounds");
+      }
+    })
 });
 
 app.get("/campgrounds/new", (req,res)=>{
