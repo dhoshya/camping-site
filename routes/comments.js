8e84c37721d34d3a77cf928b8536610c6106f5ba
Rohan Joshi <joshi.rohan09@gmail.com>
2019-04-28 13:01:24 -0400

    added edit and delete functionalityfor comments

--- routes/comments.js
+++ routes/comments.js
@@ -37,6 +37,46 @@ router.post("/", isLoggedIn, (req,res) => {
   });
 });
 
+// EDIT COMMENTS
+router.get("/:comment_id/edit", (req,res) => {
+  Comment.findById(req.params.comment_id, (err, foundComment) => {
+    if (err) {
+      console.log(req.params.comment_id);
+      res.redirect("back");
+    } else {
+      res.render("comments/edit",
+                {
+                  campground_id: req.params.id,
+                  comment: foundComment
+                });
+    }
+  });
+});
+
+// UPDATE COMMENT
+router.put("/:comment_id", (req,res) => {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment,
+                  (err, updatedComment) => {
+                    if (err) {
+                      console.log(err);
+                      res.redirect("back");
+                    } else {
+                      res.redirect("/campgrounds/" + req.params.id);
+                    }
+                  });
+});
+
+// DELETE COMMENT
+router.delete("/:comment_id", (req,res) => {
+  Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
 // MIDDLEWARE
 function isLoggedIn(req,res,next){
   if (req.isAuthenticated()) {
